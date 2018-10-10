0f3fd96da306e89ac09e05d64e241663508c5cc8
Terrel Shumway <gh5323@pythontutor.us>
2018-10-10 14:56:55 -0600

    added comments for what remains to be done

--- wordcount.py
+++ wordcount.py
@@ -10,14 +10,28 @@ class TokenT(Enum):
 Token = namedtuple("Token",['text','type'])
         
 def tokenize(text):
+    # this is NOT what I would really use: it is designed for unix shell parsing
     s = shlex(text,punctuation_chars=True)
+    s.quotes=''
+    s.escape = ''
+    s.escapedquotes = ''
+
     while True:
         t = s.get_token()
         if not t:
             break
+        # TODO: figure out whether it is a word or punctuation
+        # TODO: deal with apostrophe and abbreviations: 'twasn't a wee bairn in sight o' Dr. O'Reilly's flat.
         yield Token(t,TokenT.WORD)
 
 class Frequency(defaultdict):
+    # using defaultdict in this way allows you to replace
+    #  if foo in bar:
+    #     bar[foo]+=1
+    #  else:
+    #     bar[foo]=1
+    # with the much simpler
+    #  bar[foo]+=1
     def __missing__(self,key):
         return 0
     
@@ -28,15 +42,19 @@ def report(freq,count):
         print("{}\t{}".format(v,k.text))
 
 
-def main():
+# if you aren't familiar with click, I strongly encourage you to learn it.
+import click
+@click.command()
+@click.argument("files",nargs=-1,type=click.File('rUt'))
+def main(files):
     words = Frequency()
-    with open(__file__,'rt') as fi:
-        for t in tokenize(fi):
+    for fname in files:
+        for t in tokenize(fname):
+            # TODO: distinguish between punctuation and words
             words[t]+=1
-    report(words,20)
+    report(words,200)
 
 
 if __name__=="__main__":
     main()
 
-# this is way overcomplicated for the problem. But it will make later code easier
